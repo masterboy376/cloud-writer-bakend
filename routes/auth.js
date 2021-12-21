8a120a03301f7ba1df5f4eb5d98a80e61fde12bf
Sambhav <masterboy376@gmail.com>
2021-12-21 05:01:35 -0800

    did some changes in auth.js and created a thunder client collection to keep our requests handy.

--- routes/auth.js
+++ routes/auth.js
@@ -1,29 +1,38 @@
 const express = require('express');
-// const { Schema } = mongoose;
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 const router = express.Router();
 
 
-// create a user using: POST "/api/auth/" : Doesn't require Authentication.
-router.post('/',[
+// create a user using: POST "/api/auth/createuser". No login required.
+router.post('/createuser',[
     body('name', 'Enter a valid name...').isLength({ min: 1 }),
     body('email', 'Enter a valid email...').isEmail(),
     body('password', 'Password must contain at least 6 characters...').isLength({ min: 6 })
-] ,(req,res)=>{
+] ,async (req,res)=>{
+    // if there are errors in the body input, return bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    else{
-        User.create({
+    try{
+        //check whether the user with same email exist already
+       let user = await User.findOne({email: req.body.email});
+       if(user){
+        return res.status(400).json({ errors: "Please use different email, user with this already exists" });
+       }
+       //create a new user
+       user = await User.create({
             name: req.body.name,
             email: req.body.email,
             password: req.body.password,
-          }).then(user => res.json(user)).catch((error)=>{
-              console.log("error during creating the user. " +error);
-              res.json({error:'please enter a unique value for email...', message: error.message})
-          });
+          })
+        // send user jason as aresponse.  
+        res.json(user);
+        }
+        //this will return a error only if some internal server error occurs.
+        catch(error){
+            res.status(500).json({error: `Some error occured in the server! ERROR: ${error}`});
         }
     }
 );
